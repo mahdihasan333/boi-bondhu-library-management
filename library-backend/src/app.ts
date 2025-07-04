c9b3d233d6ee0dc3434dd06b10f0c7a7f19e03ed
Mahdi Hassan <mahdioffi333@gmail.com>
2025-07-04 18:33:15 +0600

    code update

--- library-backend/src/app.ts
+++ library-backend/src/app.ts
@@ -1,10 +1,19 @@
 import express, { Application, Request, Response } from "express";
 import { BookRoutes } from "./app/modules/book/book.route";
 import { BorrowRoutes } from "./app/modules/borrow/borrow.route";
-
+import cors from "cors";
 
 const app: Application = express();
 app.use(express.json());
+app.use(
+  cors({
+    origin: [
+      "https://library-backend-rho.vercel.app",
+      "http://localhost:5173",
+    ],
+    credentials: true,
+  })
+);
 
 app.get("/", (_req: Request, res: Response) => {
   res.send("📚 Welcome to boi-bondhu-library-management");
