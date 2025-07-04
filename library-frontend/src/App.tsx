b8036c39dd7351a428311603897898047f0b559d
Mahdi Hassan <mahdioffi333@gmail.com>
2025-07-04 15:25:41 +0600

    added footer

--- library-frontend/src/App.tsx
+++ library-frontend/src/App.tsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Outlet } from "react-router";
+// import { Nav } from "./components/layout/Nav";
+import { Footer } from "./components/layout/Footer";
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      {/* <Nav /> */}
+      <div className="w-11/12 mx-auto lg:w-11/12 md:w-11/12 xl:container">
+        <div className="min-h-[calc(100vh-441px)]">
+          <Outlet />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
